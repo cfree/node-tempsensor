145377e987fec37ba0a300ab54fdeb7e3f689577
Craig Freeman <cfreeman@twilio.com>
2019-06-04 22:44:28 -0700

    Update URL

--- src/report.js
+++ src/report.js
@@ -1,13 +1,7 @@
 const fetch = require('node-fetch');
-const { URLSearchParams } = require('url');
 
 function reportTemp(temp, threshold, timestamp) {
-  const params = new URLSearchParams();
-  params.append('temp', temp);
-  params.append('threshold', threshold);
-  params.append('timestamp', timestamp);
-
-  return fetch(process.env.REPORT_URL, { body: params })
+  return fetch(`${process.env.REPORT_URL}?temp=${temp}&threshold=${threshold}&timestamp=${timestamp}`)
     .then((res) => res.json())
     .then((data) => {
       console.log(`Reported: ${temp}deg F at ${timestamp}. Message: ${data}`);
