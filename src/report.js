e51adfe1b1205b110f4bc98de7fc80701759bf51
Craig Freeman <craigfreeman@gmail.com>
2019-10-21 15:54:28 -0700

    Remove message data

--- src/report.js
+++ src/report.js
@@ -2,10 +2,8 @@ const fetch = require('node-fetch');
 
 function reportTemp(temp, threshold, timestamp) {
   return fetch(`${process.env.REPORT_URL}?temp=${temp}&threshold=${threshold}&timestamp=${timestamp}`)
-    .then((res) => res.json())
     .then((data) => {
-      console.log(`Reported: ${temp}deg F at ${timestamp}. Message: ${data}`);
-      return data;
+      console.log(`Reported: ${temp}deg F at ${timestamp}.`);
     })
     .catch(console.error);
 }
